0edc58acc66060530008147cc17958392375b355
amine amara <aamara@sephora.fr>
2021-04-27 01:22:49 +0200

    reponsive interface

--- GameInterface.gd
+++ GameInterface.gd
@@ -3,9 +3,9 @@ extends Node2D
 func _ready():
 	for i in GameManager.deck:
 		if i == "1-3":
-			get_node("Footer_interface/ColorRect/Grid").add_child(Card_kill.new(i, "toto"))
+			get_node("Camera2D/Control/GridContainer").add_child(Card_kill.new(i, "toto"))
 		else:
-			get_node("Footer_interface/ColorRect/Grid").add_child(Card.new(i, "tata"))
+			get_node("Camera2D/Control/GridContainer").add_child(Card.new(i, "tata"))
 
 #func _input(event):
 #	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
